b924715d046acfe10c63a123bfa8037801da438a
n3rd-asc <n3rd.asc@gmail.com>
2023-06-07 17:17:54 +0200

    audioPlayer.html bottomSheet licenseMenu add + bug fix

--- assets/js/bottomMenu.js
+++ assets/js/bottomMenu.js
@@ -1,15 +1,37 @@
 // AFFICHAGE DE LA FENÊTRE DIALOGUE AU CLIC sur .dotLink
 document.querySelector('.dotLink').addEventListener('click', function () {
     var bottomSheet = document.getElementById('bottomSheet');
+
+    // var LicensebottomSheet = document.querySelector('.licenceBottomSheet');
+    // var licenseOverlay = document.getElementById('licenseOverlay');
+
+    // LicensebottomSheet.style.display ="none";
+    // licenseOverlay.style.display ="none";
+    
     bottomSheet.classList.toggle('open');
     var overlay = document.getElementById('overlay');
     overlay.classList.toggle('hideShowOverlay');
 });
 
+// AFFICHAGE DE LA FENÊTRE DIALOGUE AU CLIC sur Licence/Source
+document.querySelector('.trackLicense').addEventListener('click', function () {
+    var LicensebottomSheet = document.querySelector('.licenceBottomSheet');
+    LicensebottomSheet.classList.toggle('open');
+    var licenseOverlay = document.getElementById('licenseOverlay');
+    licenseOverlay.classList.toggle('hideShowOverlay');
+});
+
 // DISPARITION DE LA FENÊTRE DIALOGUE AU CLIC SUR .overlay
 document.querySelector('.overlay').addEventListener('click', function () {
-    var bottomSheet = document.getElementById('bottomSheet');
     bottomSheet.classList.toggle('open');
     var overlay = document.getElementById('overlay');
     overlay.classList.toggle('hideShowOverlay');
+});
+
+// DISPARITION DE LA FENÊTRE DIALOGUE AU CLIC SUR .licenseOverlay
+document.getElementById('licenseOverlay').addEventListener('click', function () {
+    var LicensebottomSheet = document.querySelector('.licenceBottomSheet');
+    LicensebottomSheet.classList.toggle('open');
+    var licenseOverlay = document.getElementById('licenseOverlay');
+    licenseOverlay.classList.toggle('hideShowOverlay');
 });
